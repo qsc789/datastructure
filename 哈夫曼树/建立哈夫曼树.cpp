f2adef4f03892afd5d7987d8f48e3a0f59b15a7a
qsc789 <1279495882@qq.com>
2024-04-16 11:32:01 +0800

    建立哈夫曼树

--- 哈夫曼树/建立哈夫曼树.cpp
+++ 哈夫曼树/建立哈夫曼树.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Node {
+#include<iostream>
+using namespace std;
+typedef struct Node {
     char data;
     int freq;
     struct Node* left;
     struct Node* right;
-};
+}Node;
 
-typedef struct Node Node;
+
 
 Node* createNode(char data, int freq) {
     Node* newNode = (Node*)malloc(sizeof(Node));
@@ -45,15 +46,25 @@ Node* buildHuffmanTree(char data[], int freq[], int n) {
 
     return nodes[0];
 }
-
+void printPreOrder(Node* root)
+{
+    if(root==NULL)
+    {
+        return ;
+    }
+    cout<<root->data<<' '<<root->freg<<endl;
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+}
 int main() {
     char data[] = {'a', 'b', 'c', 'd', 'e'};
-    int freq[] = {5, 9, 12, 13, 16};
+    int freq[] = {5, 9, 12, 13, 16};//权值,从小到大
     int n = sizeof(data) / sizeof(data[0]);
 
     Node* root = buildHuffmanTree(data, freq, n);
 
     // 这里可以添加打印哈夫曼树的代码
+    void printPreOrder(&root);
 
     return 0;
 }
