f3b2a4d1ab4081f1093a5b1764466d0ca9ffb87a
qsc789 <1279495882@qq.com>
2024-04-18 11:34:19 +0800

    建立邻接图和邻接矩阵和逆邻接矩阵

--- 图/邻接图和邻接矩阵/邻接图和邻接矩阵.cpp
+++ 图/邻接图和邻接矩阵/邻接图和邻接矩阵.cpp
@@ -1,59 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VERTICES 100
+#define MAX_VERTICES 5
 
 // 邻接矩阵
 int adjacencyMatrix[MAX_VERTICES][MAX_VERTICES];
 
-// 邻接链表
-struct Node {
+// 邻接链表的节点结构体
+typedef struct Node {
     int vertex;
     struct Node* next;
-};
+} Node;
 
-struct Graph {
-    struct Node* adjList[MAX_VERTICES];
-};
+// 邻接链表数组
+Node* adjacencyList[MAX_VERTICES];
 
-void addEdgeMatrix(int v1, int v2) {
-    adjacencyMatrix[v1][v2] = 1;
-    adjacencyMatrix[v2][v1] = 1;
+// 逆邻接链表数组
+Node* inverseAdjacencyList[MAX_VERTICES];
+
+// 初始化图的邻接矩阵
+void initAdjacencyMatrix() {
+    for (int i = 0; i < MAX_VERTICES; i++) {
+        for (int j = 0; j < MAX_VERTICES; j++) {
+            adjacencyMatrix[i][j] = 0;
+        }
+    }
+
+    adjacencyMatrix[0][1] = 1;
+    adjacencyMatrix[0][3] = 1;
+    adjacencyMatrix[1][2] = 1;
+    adjacencyMatrix[2][3] = 1;
+    adjacencyMatrix[3][4] = 1;
 }
 
-void displayAdjacencyMatrix(int numVertices) {
-    printf("Adjacency Matrix:\n");
+// 添加边到邻接链表
+void addEdgeToList(int v1, int v2) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = v2;
+    newNode->next = adjacencyList[v1];
+    adjacencyList[v1] = newNode;
+}
+
+// 生成逆邻接链表
+void generateInverseAdjacencyList(int numVertices) {
+    for (int i = 0; i < numVertices; i++) {
+        inverseAdjacencyList[i] = NULL;
+    }
+
     for (int i = 0; i < numVertices; i++) {
         for (int j = 0; j < numVertices; j++) {
-            printf("%d ", adjacencyMatrix[i][j]);
+            if (adjacencyMatrix[j][i] == 1) {
+                // 在逆邻接链表中添加边的起点
+                Node* newNode = (Node*)malloc(sizeof(Node));
+                newNode->vertex = j;
+                newNode->next = inverseAdjacencyList[i];
+                inverseAdjacencyList[i] = newNode;
+            }
+        }
+    }
+}
+
+// 打印邻接链表
+void printAdjacencyList(int numVertices) {
+    printf("Adjacency List:\n");
+    for (int i = 0; i < numVertices; i++) {
+        printf("%d -> ", i);
+        Node* current = adjacencyList[i];
+        while (current != NULL) {
+            printf("%d ", current->vertex);
+            current = current->next;
         }
         printf("\n");
     }
 }
 
-struct Node* createNode(int v) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->vertex = v;
-    newNode->next = NULL;
-    return newNode;
-}
-
-void addEdgeList(struct Graph* graph, int v1, int v2) {
-    struct Node* newNode = createNode(v2);
-    newNode->next = graph->adjList[v1];
-    graph->adjList[v1] = newNode;
-
-    newNode = createNode(v1);
-    newNode->next = graph->adjList[v2];
-    graph->adjList[v2] = newNode;
-}
-
-void displayAdjacencyList(struct Graph* graph, int numVertices) {
-    printf("Adjacency List:\n");
+// 打印逆邻接链表
+void printInverseAdjacencyList(int numVertices) {
+    printf("Inverse Adjacency List:\n");
     for (int i = 0; i < numVertices; i++) {
         printf("%d -> ", i);
-        struct Node* current = graph->adjList[i];
-        while (current) {
+        Node* current = inverseAdjacencyList[i];
+        while (current != NULL) {
             printf("%d ", current->vertex);
             current = current->next;
         }
@@ -62,25 +89,32 @@ void displayAdjacencyList(struct Graph* graph, int numVertices) {
 }
 
 int main() {
-    int numVertices = 4;
-    struct Graph graph;
+    initAdjacencyMatrix();
+
+    int numVertices = 5;
+
+    // 初始化邻接链表和逆邻接链表
     for (int i = 0; i < numVertices; i++) {
-        graph.adjList[i] = NULL;
+        adjacencyList[i] = NULL;
+        inverseAdjacencyList[i] = NULL;
     }
 
-    addEdgeMatrix(0, 1);
-    addEdgeMatrix(0, 2);
-    addEdgeMatrix(1, 3);
-    addEdgeMatrix(2, 3);
+    // 生成邻接链表
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
+            if (adjacencyMatrix[i][j] == 1) {
+                addEdgeToList(i, j);
+            }
+        }
+    }
 
-    displayAdjacencyMatrix(numVertices);
+    // 生成逆邻接链表
+    generateInverseAdjacencyList(numVertices);
 
-    addEdgeList(&graph, 0, 1);
-    addEdgeList(&graph, 0, 2);
-    addEdgeList(&graph, 1, 3);
-    addEdgeList(&graph, 2, 3);
-
-    displayAdjacencyList(&graph, numVertices);
+    // 打印邻接链表和逆邻接链表
+    printAdjacencyList(numVertices);
+    printInverseAdjacencyList(numVertices);
 
     return 0;
 }
+
